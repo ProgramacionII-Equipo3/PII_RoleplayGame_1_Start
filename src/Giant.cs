44680a960b29d09d006046f08400fff4179e64fb
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-09-14 20:15:27 -0300

    Classes Added, updated CRC.json

---
+++ src/Giant.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Character
+{
+    private string Name;
+    private uint Damage;
+    private uint Health;
+    public bool Hammer;
+    public bool Armor;
+
+    public class Giant
+    {
+        public Giant()
+        {
+            this.Name = "Giant";
+            this.Damage = 40;
+            this.Health = 150;
+            this.Hammer = False;
+            this.Armor = False;
+        }
+
+        public Attack(Giant giant, )
+        {
+
+        }
+    }
+}
