3be41c4fa59b5b69e385626aa42b999abf836d9a
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-09-17 14:56:59 -0300

    Add unit test

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -1,16 +1,29 @@
 using NUnit.Framework;
+using Library;
 
-namespace Test.Library
+namespace Library.Test
 {
 
-
+    [TestFixture]
     public class ExampleTest
     {
 
         [Test]
-        public void dummyTest()
+        public void Test1()
         {
-            Assert.True(true);
+            Dwarf dwarf = new Dwarf("Patricio", 500, 30, 20);
+            dwarf.Weapon = new Axe(50, 0);
+            dwarf.Shield = new Shield(0, 100, 50);
+
+            Wizard wizard = new Wizard("Fernando", 300, "Fire", 40);
+            wizard.Wand = new MagicWand(100, 20);
+            wizard.Book = new SpellBook(500, 0);
+
+            wizard.ReceiveAttack(dwarf.NetStrength);
+            Assert.AreEqual(wizard.CurrentVit, 240);
+
+            dwarf.ReceiveAttack(wizard.NetStrength);
+            Assert.AreEqual(dwarf.CurrentVit, 0);
         }
 
     }
